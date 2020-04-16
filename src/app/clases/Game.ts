879780c88343c986314dae6203d94402fbf5f66c
Damian Jorda <damian.jjorda@gmail.com>
2020-04-16 01:35:03 -0300

    Fixes

--- src/app/clases/Game.ts
+++ src/app/clases/Game.ts
@@ -1,5 +1,22 @@
 export class Game {
 
-  constructor(private imagen: string, private nombre: string, private descripcion: string, private routing: string) { }
+  constructor(private _imagen: string, private _nombre: string, private _descripcion: string, private _routing: string) {
+  }
+
+  public get imagen(): string {
+    return this._imagen;
+  }
+
+  public get routing(): string {
+    return this._routing;
+  }
+
+  public get descripcion(): string {
+    return this._descripcion;
+  }
+
+  public get nombre(): string {
+    return this._nombre;
+  }
 
 }
