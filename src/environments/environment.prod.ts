9a0ebcbc697279880fc572fba4b965c3fc436af0
Damian Jorda <damian.jjorda@gmail.com>
2020-05-03 23:10:50 -0300

    Added variable to environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDM-dCQ-ammF6hUYpkV0Ad6by2-M5yyD48',
+    authDomain: 'tp-saladejuegosdamianjorda.firebaseapp.com',
+    databaseURL: 'https://tp-saladejuegosdamianjorda.firebaseio.com',
+    projectId: 'tp-saladejuegosdamianjorda',
+    storageBucket: 'tp-saladejuegosdamianjorda.appspot.com',
+    messagingSenderId: '24636219952',
+    appId: '1:24636219952:web:61ea89cfd803190a2283f5',
+    measurementId: 'G-25YM8TN42J'
+  }
 };
