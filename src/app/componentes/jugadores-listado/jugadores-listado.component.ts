a6d5ebbce94e5370c09ea9ae76494e044c1be569
Damian Jorda <djorda@quadiontech.com>
2020-05-18 21:00:56 -0300

    jugadores list funciona

--- src/app/componentes/jugadores-listado/jugadores-listado.component.ts
+++ src/app/componentes/jugadores-listado/jugadores-listado.component.ts
@@ -7,7 +7,6 @@ import { JugadoresService } from '../../servicios/jugadores.service';
   styleUrls: ['./jugadores-listado.component.css']
 })
 export class JugadoresListadoComponent implements OnInit {
-
   listado: any;
   miJugadoresServicio: JugadoresService;
 
@@ -22,11 +21,13 @@ export class JugadoresListadoComponent implements OnInit {
       this.listado = data;
     });
   }
+
   traerGanadores() {
     this.miJugadoresServicio.traertodos('jugadores/', 'ganadores').then(data => {
       this.listado = data;
     });
   }
+  
   traerPerdedores() {
     this.miJugadoresServicio.traertodos('jugadores/', 'perdedores').then(data => {
       this.listado = data;
--- src/app/servicios/jugadores.service.ts
+++ src/app/servicios/jugadores.service.ts
@@ -1,32 +1,19 @@
 import { Injectable } from '@angular/core';
+import { take } from 'rxjs/operators';
+import { PartidaService } from './partida.service';
 @Injectable()
 export class JugadoresService {
 
-  //peticion:any;
-  constructor() {
-    // this.peticion = this.miHttp.traerJugadores();
-    //    this.peticion = this.miHttp.httpGetO("https://restcountries.eu/rest/v2/all");
+  constructor(private partidaService: PartidaService) {
   }
 
-
-  jugadoresFiltrados: any;
-
   traertodos(ruta: string, filtro: string): Promise<any> {
-    // return this.miHttp.traerJugadores(ruta).then(jugadores => {
-    //   console.info('jugadores service', jugadores);
-
-    //   this.jugadoresFiltrados = jugadores;
-    //   let ganador: boolean;
-    //   ganador = filtro === 'ganadores';
-
-    //   this.jugadoresFiltrados = this.jugadoresFiltrados.filter(unJugador => unJugador.gano === ganador || filtro === 'todos');
-    //   return this.jugadoresFiltrados;
-    // })
-    //   .catch(errror => {
-    //     console.log('error');
-    //     return this.jugadoresFiltrados;
-    //   });
-    return new Promise((resolve, reject) => { });
+    return this.partidaService.obtenerPartidas()
+      .map(partidas => {
+        let ganador: boolean;
+        ganador = filtro === 'ganadores';
+        return partidas.filter(partida => partida.gano === ganador || filtro === 'todos');
+      }).pipe(take(1)).toPromise();
   }
 
 }
